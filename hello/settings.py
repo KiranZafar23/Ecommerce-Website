c67bebf9f6f4fb51b2cba5dbf0b275fc0042738b
Kiran Zafar <91126895+KiranZafar23@users.noreply.github.com>
2022-09-16 13:24:33 +0500

    Update settings.py

--- hello/settings.py
+++ hello/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from os import path
 from pathlib import Path
 
@@ -128,9 +129,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Added Manually
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
